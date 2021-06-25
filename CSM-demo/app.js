c945cbcfe40f6202e05ff3314254697604a62370
tkhresk <takahiro.esaki@ibm.com>
2021-06-25 17:59:14 +0900

    changed_02

--- CSM-demo/app.js
+++ CSM-demo/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!-0625-Esaki-01')
+  res.send('Hello World!-0625-Esaki-02')
 })
 
 app.listen(port, () => {
